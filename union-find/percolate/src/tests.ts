db233b708629a8579bc71b04400df8bd7f5220f7
Greg Hurrell <greg@hurrell.net>
2020-10-04 19:25:35 +0200

    test: show that percolation detection is probably right
    When I wrote it, I was a bit worried about sensitivity to ordering
    issues, because the order in which we open cells determines how leaders
    are chosen over time, and how ranks change etc, all of which could
    invalidate our assumption that we can reliably look up the "lowest"
    entry for a given leader.
    
    This test isn't exhaustive (in the sense that it only opens cells in the
    first column), but it it does test all permutations (tens of thousands
    of them) in for an 8x8 grid, so it can give us some confidence that the
    solution probably works.

--- union-find/percolate/src/tests.ts
+++ union-find/percolate/src/tests.ts
@@ -1,6 +1,8 @@
 import assert from 'assert';
 
+import Percolation from './Percolation';
 import UnionFind from './UnionFind';
+import permute from './permute';
 
 const uf = new UnionFind(10);
 
@@ -20,3 +22,28 @@ assert.equal(uf.find(7), 7);
 
 assert.equal(uf.find(0), 0);
 assert.equal(uf.find(6), 6);
+
+// Show that percolation testing works regardless of order.
+
+assert.deepEqual(permute([0, 1, 2]), [
+	[0, 1, 2],
+	[1, 0, 2],
+	[2, 0, 1],
+	[0, 2, 1],
+	[1, 2, 0],
+	[2, 1, 0],
+]);
+
+permute([0, 1, 2, 3, 4, 5, 6, 7]).forEach((rows) => {
+	const p = new Percolation(8);
+
+	for (let i = 0; i < 7; i++) {
+		p.open(rows[i], 0);
+	}
+
+	assert.equal(p.percolates(), false);
+
+	p.open(rows[7], 0);
+
+	assert.equal(p.percolates(), true);
+});

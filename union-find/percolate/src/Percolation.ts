6aa386f2231426a252a172ab35dd504ce709dfe7
Greg Hurrell <greg.hurrell@liferay.com>
2020-10-09 09:48:33 +0200

    chore: remove implementation details from sample branch

--- union-find/percolate/src/Percolation.ts
+++ union-find/percolate/src/Percolation.ts
@@ -1,139 +1,51 @@
 import UnionFind from './UnionFind';
 
-const BLOCKED = '🛑';
-const EMPTY = '  ';
-const FULL = '💧';
-
 export default class Percolation {
-	_cells: Array<boolean>;
-	_connections: UnionFind;
-	_lowest: Map<number, number>;
-	_open: number;
-	_percolates: boolean;
 	_size: number;
-	_top: number;
 
 	/**
 	 * Creates n-by-n grid, with all sites initially blocked.
 	 */
 	constructor(size: number) {
-		this._cells = new Array(size * size);
-		this._connections = new UnionFind(size * size + 1);
-		this._lowest = new Map();
-		this._open = 0;
-		this._percolates = false;
 		this._size = size;
-
-		// Additional virtual site for linking to at top.
-
-		this._top = this._connections.size - 1;
 	}
 
 	/**
 	 * Opens the site (row, column) if it is not open already.
 	 */
 	open(row: number, column: number) {
-		const index = row * this._size + column;
-
-		if (this._cells[index]) {
-			return true;
-		}
-
-		this._cells[index] = true;
-		this._open++;
-
-		// Link with open neighbors.
-
-		if (row === 0) {
-			this._connections.union(index, this._top);
-		}
-
-		if (row > 0 && this.isOpen(row - 1, column)) {
-			this._connections.union(index, (row - 1) * this._size + column);
-		}
-
-		if (row < this._size - 1 && this.isOpen(row + 1, column)) {
-			this._connections.union(index, (row + 1) * this._size + column);
-		}
-
-		if (column > 0 && this.isOpen(row, column - 1)) {
-			this._connections.union(index, row * this._size + column - 1);
-		}
-
-		if (column < this._size - 1 && this.isOpen(row, column + 1)) {
-			this._connections.union(index, row * this._size + column + 1);
-		}
-
-		// Detect percolation.
-		const leader = this._connections.find(index);
-
-		let lowest = this._lowest.get(leader);
-
-		if (lowest === undefined || index > lowest) {
-			this._lowest.set(leader, index);
-
-			lowest = index;
-		}
-
-		if (
-			Math.floor(lowest / this._size) === this._size - 1 &&
-			this.isFull(Math.floor(lowest / this._size), lowest % this._size)
-		) {
-			this._percolates = true;
-		}
+		// TODO...
 	}
 
 	/**
 	 * Is the site (row, column) open?
 	 */
 	isOpen(row: number, column: number) {
-		return !!this._cells[row * this._size + column];
+		return false; // TODO
 	}
 
 	/**
 	 * Is the site (row, column) full?
 	 */
 	isFull(row: number, column: number): boolean {
-		const index = row * this._size + column;
-
-		return (
-			this._connections.find(index) === this._connections.find(this._top)
-		);
+		return false; // TODO
 	}
 
 	/**
 	 * Returns the number of open sites.
 	 */
 	numberOfOpenSites(): number {
-		return this._open;
+		return 0; // TODO
 	}
 
 	/**
 	 * Does the system percolate?
 	 */
 	percolates(): boolean {
-		return this._percolates;
+		return true; // TODO
 	}
 
 	toString() {
-		let grid = '┏' + '━'.repeat(this._size * 2) + '┓\n';
-
-		for (let row = 0; row < this._size; row++) {
-			grid += '┃';
-
-			for (let column = 0; column < this._size; column++) {
-				if (this.isFull(row, column)) {
-					grid += FULL;
-				} else if (this.isOpen(row, column)) {
-					grid += EMPTY;
-				} else {
-					grid += BLOCKED;
-				}
-			}
-			grid += '┃\n';
-		}
-		grid += '┗' + '━'.repeat(this._size * 2) + '┛\n';
-
-		return grid;
+		return ''; // TODO (optional, for debugging purposes)
 	}
 }
--- union-find/percolate/src/tests.ts
+++ union-find/percolate/src/tests.ts
@@ -23,27 +23,8 @@ assert.equal(uf.find(7), 7);
 assert.equal(uf.find(0), 0);
 assert.equal(uf.find(6), 6);
 
-// Show that percolation testing works regardless of order.
+// TODO: write some real percolation tests...
 
-assert.deepEqual(permute([0, 1, 2]), [
-	[0, 1, 2],
-	[1, 0, 2],
-	[2, 0, 1],
-	[0, 2, 1],
-	[1, 2, 0],
-	[2, 1, 0],
-]);
+const p = new Percolation(8);
 
-permute([0, 1, 2, 3, 4, 5, 6, 7]).forEach((rows) => {
-	const p = new Percolation(8);
-
-	for (let i = 0; i < 7; i++) {
-		p.open(rows[i], 0);
-	}
-
-	assert.equal(p.percolates(), false);
-
-	p.open(rows[7], 0);
-
-	assert.equal(p.percolates(), true);
-});
+assert.equal(p.percolates(), true);
